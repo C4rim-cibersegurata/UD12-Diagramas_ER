be222860ef36ad79b5c4daf0b7638ba5e6581d9f
C4rim-cibersegurata <114733605+C4rim-cibersegurata@users.noreply.github.com>
2022-10-26 17:47:43 +0200

    Update EX7.sql

--- UD14 DDL/EX7.sql
+++ UD14 DDL/EX7.sql
@@ -10,11 +10,13 @@ CREATE TABLE `Organigrama`.`trabajador` (
   `Telefono` INT NULL,
   `Responsable` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`IdCodeTrabajador`));
-CREATE TABLE departamento (IdCodeDepartamento INT NOT NULL AUTO_INCREMENT, 
+CREATE TABLE departamento (
+IdCodeDepartamento INT NOT NULL AUTO_INCREMENT, 
 nombre VARCHAR(45) NOT NULL,
 PRIMARY KEY (IdCodeDepartamento));
 
-CREATE TABLE trabajadorDepartamento (IdCodeTrabajador INT REFERENCES trabajador (IdCodeTrabajador) on delete cascade on update cascade,
+CREATE TABLE trabajadorDepartamento (
+IdCodeTrabajador INT REFERENCES trabajador (IdCodeTrabajador) on delete cascade on update cascade,
 IdCodeDepartamento INT REFERENCES departamento (IdCodeDepartamento) on delete cascade on update cascade,
 PRIMARY KEY(IdCodeTrabajador, IdCodeDepartamento)
 );
@@ -30,15 +32,43 @@ fecha DATE NOT NULL,
 salario INT NOT NULL,
 idContrato INT REFERENCES contrato (idContrato) on delete cascade on update cascade
 );
-INSERT INTO `trabajador` (`IdCodeTrabajador`,`Dni`,`NumSeguridadSocial`,`Nombre`,`Apellidos`,`Direccion`,`Telefono`,`Responsable`)
+INSERT INTO `departamento` (`nombre`) VALUES  ("Compras"), ("Legal"), ("Personal"), ("Programación"), ("Seguridad"), ("Operaciones"), ("Sistemas"), ("RRHH"), ("Marketing"), ("Contabilidad");
+
+INSERT INTO `contrato` (`fechainicio`,`fechafinal`)
 VALUES
-  (1,"7147377D","349486668","Renee","Marks","725-8116 Dolor St.","621272758","0"),
-  (2,"9685753K","538644942","Hilary","Bryan","Ap #456-806 Cras Av.","692741833","0"),
-  (3,"3684797I","223353872","Victor","Harrison","Ap #599-4767 Mi Ave","636327966","0"),
-  (4,"8392557W","486336551","Kennedy","Nunez","729-4921 Egestas Road","659276729","0"),
-  (5,"3636462B","272766322","Maggy","Mcclain","102-4904 Dictum. St.","672648351","0"),
-  (6,"9836614V","741555288","Brent","Benson","5491 Non Rd.","668664283","0"),
-  (7,"6274624X","348427857","Martina","Farley","7087 Ante, Road","634358393","0"),
-  (8,"3871252C","478352593","Dara","Patel","Ap #719-2878 Nibh. Road","623626513","0"),
-  (9,"5288226D","214998332","Cameron","Cline","Ap #822-8019 Ante Av.","664326143","0"),
-  (10,"2473523L","625473856","Rosalyn","Vega","6184 Id Avenue","625559825","0");
+  ("01/20/22","10/22/22"),
+  ("10/24/22","03/27/22"),
+  ("11/30/22","12/25/21"),
+  ("04/13/22","09/09/22"),
+  ("01/04/23","11/26/21"),
+  ("08/24/22","03/02/22"),
+  ("12/21/22","11/10/21"),
+  ("08/22/22","06/06/22"),
+  ("12/08/22","10/29/21"),
+  ("05/26/22","09/28/22");
+
+INSERT INTO `nomina` (`fecha`,`salario`)
+VALUES
+  ("01/20/22",1100),
+  ("10/24/22",930),
+  ("11/30/22",850),
+  ("04/13/22",1200),
+  ("01/04/23",2100),
+  ("08/24/22",1090),
+  ("12/21/22",910),
+  ("08/22/22",1066),
+  ("12/08/22",1560),
+  ("05/26/22",12000);
+
+INSERT INTO `trabajador` (`Dni`,`NumSeguridadSocial`,`Nombre`,`Apellidos`,`Direccion`,`Telefono`,`Responsable`)
+VALUES
+  ("7147377D","349486668","Renee","Marks","725-8116 Dolor St.","621272758","0"),
+  ("9685753K","538644942","Hilary","Bryan","Ap #456-806 Cras Av.","692741833","0"),
+  ("3684797I","223353872","Victor","Harrison","Ap #599-4767 Mi Ave","636327966","0"),
+  ("8392557W","486336551","Kennedy","Nunez","729-4921 Egestas Road","659276729","0"),
+  ("3636462B","272766322","Maggy","Mcclain","102-4904 Dictum. St.","672648351","0"),
+  ("9836614V","741555288","Brent","Benson","5491 Non Rd.","668664283","0"),
+  ("6274624X","348427857","Martina","Farley","7087 Ante, Road","634358393","0"),
+  ("3871252C","478352593","Dara","Patel","Ap #719-2878 Nibh. Road","623626513","0"),
+  ("5288226D","214998332","Cameron","Cline","Ap #822-8019 Ante Av.","664326143","0"),
+  (1"2473523L","625473856","Rosalyn","Vega","6184 Id Avenue","625559825","0");
